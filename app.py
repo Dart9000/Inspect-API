1a4aafea44147062513ef19dbf8569f196641e30
Garvit9000c <909garvit@gmail.com>
2022-01-02 09:24:55 +0530

    init

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request
+from flask_cors import CORS
 import api
 
 app = Flask(__name__)
+CORS(app)
+
 
 # To run python file without re-running the flask command
 def before_request():
